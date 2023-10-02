6fea4a84c779f925e74549a9c8bd382af6ac8fea
Paige Dudkiewicz <paigedudkiewicz@Paiges-MacBook-Pro.local>
2023-10-02 16:42:11 -0400

    Finished hero list with detailed view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,9 +5,13 @@ import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
-
   heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(hero);
+  }
 }
