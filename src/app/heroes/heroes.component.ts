d5d5aa4964bb7dcc3245a15e141dfc6bd3c1ee8f
Paige Dudkiewicz <paigedudkiewicz@Paiges-MacBook-Pro.local>
2023-10-17 21:29:44 -0400

    added hero detail component, hero service, message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +8,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     console.log(hero);
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((newHeroes) => {
+      this.heroes = newHeroes;
+    });
   }
 }
