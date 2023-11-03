2b735db38c96ed0efb9018bcedba9b01565d35c8
Paige Dudkiewicz <paigedudkiewicz@Paiges-MacBook-Pro.local>
2023-11-03 12:17:24 -0400

    Created S3 bucket and gitHub action for deployment

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,27 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
-  constructor(private heroService: HeroService) {}
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    console.log(hero);
-  }
+
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe((newHeroes) => {
-      this.heroes = newHeroes;
-    });
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
